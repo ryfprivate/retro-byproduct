c3d7f76a13b226e085dde869f9871b0d1014e323
ryfprivate <ryfprivate@outlook.com>
2021-03-08 15:17:35 +1100

    Added basic player movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    public InputAction wasd;
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    Vector2 movement;
+
+    void OnEnable() {
+        wasd.Enable();
+    }
+
+    void onDisable() {
+        wasd.Disable();
+    }
+    
+    void Update() {
+        movement = wasd.ReadValue<Vector2>();
+        Debug.Log(movement);
+    }
+
+    void FixedUpdate()
+    {
+        // Movement
+        Debug.Log(movement);
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
