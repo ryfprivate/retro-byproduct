01f42981f6c7d26710696a44ccffebc5728f8c70
ryfprivate <ryfprivate@outlook.com>
2021-03-09 16:25:56 +1100

    Added camera following player

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset;
+
+    void Update()
+    {
+        transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, -10f); // Camera follows the player with specified offset position
+    }
+}
