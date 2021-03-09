3ee887d2002c5903abed1ce8fa7a191df3298fb7
ryfprivate <ryfprivate@outlook.com>
2021-03-09 13:07:28 +1100

    Renamed player variables

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,15 +6,16 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     public InputAction moveInput;
-    public InputAction aimInput;
+    public InputAction shootInput;
+
     public Rigidbody2D rb;
     public Animator animator;
     public Transform aimUI;
 
     public float moveSpeed = 5f;
 
-    Vector2 movement;
-    Vector2 aim;
+    Vector2 moveVector;
+    Vector2 aimVector;
 
     // Shooting
     public Transform firePoint;
@@ -22,30 +23,33 @@ public class Player : MonoBehaviour
 
     public float bulletForce = 20f;
 
-    void OnEnable() {
+    void OnEnable()
+    {
         moveInput.Enable();
-        aimInput.Enable();
+        shootInput.Enable();
     }
 
-    void onDisable() {
+    void onDisable()
+    {
         moveInput.Disable();
-        aimInput.Disable();
+        shootInput.Disable();
     }
-    
-    void Update() {
-        movement = moveInput.ReadValue<Vector2>();
-        aim = aimInput.ReadValue<Vector2>();
-        float angle = Vector3.SignedAngle(new Vector2(0, -1), aim, Vector3.forward);
+
+    void Update()
+    {
+        moveVector = moveInput.ReadValue<Vector2>();
+        aimVector = shootInput.ReadValue<Vector2>();
+        float angle = Vector3.SignedAngle(new Vector2(0, -1), aimVector, Vector3.forward);
         aimUI.rotation = Quaternion.Euler(0, 0, angle);
 
-        animator.SetFloat("Horizontal", movement.x);
-        animator.SetFloat("Vertical", movement.y);
-        animator.SetFloat("Speed", movement.sqrMagnitude);
+        animator.SetFloat("Horizontal", moveVector.x);
+        animator.SetFloat("Vertical", moveVector.y);
+        animator.SetFloat("Speed", moveVector.sqrMagnitude);
     }
 
     void FixedUpdate()
     {
-        // Movement
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        // Player Movement
+        rb.MovePosition(rb.position + moveVector * moveSpeed * Time.fixedDeltaTime);
     }
 }
