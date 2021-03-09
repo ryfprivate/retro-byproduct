b3173dd1c84664e9bd581cffeb1ef8743076e919
ryfprivate <ryfprivate@outlook.com>
2021-03-09 18:52:44 +1100

    Imported CodeMonkey Utils

---
+++ Assets/Scripts/Testing.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    void Start()
+    {
+        Grid grid = new Grid(20, 10);
+
+
+    }
+}
