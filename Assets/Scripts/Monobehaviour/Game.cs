4af0e629a14ff71412bb2c31250538a62b9a1dd7
ryfprivate <ryfprivate@outlook.com>
2021-03-10 14:54:08 +1100

    Stored instance of pathfinding in game

--- Assets/Scripts/Monobehaviour/Game.cs
+++ Assets/Scripts/Monobehaviour/Game.cs
@@ -1,21 +1,61 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using CodeMonkey.Utils;
 
 public class Game : MonoBehaviour
 {
     public GameObject playerPrefab;
-    public Transform playerParent;
-    public Vector3 spawnPosition;
+
+    // Class Instances
+    private GameControls controls;
+    private Pathfinding _pathfinding;
+
+    void OnEnable()
+    {
+        controls.Enable();
+        controls.Player.LeftClick.performed += ctx => HandleLeftClick(ctx);
+        // controls.Player.RightClick.performed += ctx => HandleRightClick(ctx);
+    }
+    void OnDisable()
+    {
+        controls.Disable();
+        controls.Player.LeftClick.performed -= ctx => HandleLeftClick(ctx);
+        // controls.Player.RightClick.performed -= ctx => HandleRightClick(ctx);
+    }
+
+    void Awake() {
+        controls = new GameControls();
+        _pathfinding = new Pathfinding(25, 25);
+    }
+
 
     void Start()
     {
-        spawnPosition = new Vector3(15, 15, 0);
-        Instantiate(playerPrefab, spawnPosition, playerParent.rotation, playerParent);
+        Vector3 spawnPosition = new Vector3(15, 15, 0);
+        Instantiate(playerPrefab, spawnPosition, Quaternion.Euler(Vector3.zero));
     }
 
-    void Update()
+    private void HandleLeftClick(InputAction.CallbackContext ctx)
     {
-
+        Vector2 screenPosition = Mouse.current.position.ReadValue();
+        Vector2 worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
+        _pathfinding.GetGrid().GetXY(worldPosition, out int x, out int y);
+        List<PathNode> path = _pathfinding.FindPath(0, 0, x, y);
+        if (path != null)
+        {
+            for (int i = 0; i < path.Count - 1; i++)
+            {
+                Debug.DrawLine(new Vector3(path[i].x, path[i].y) + Vector3.one * .5f, new Vector3(path[i + 1].x, path[i + 1].y) + Vector3.one * .5f, Color.green, .5f);
+            }
+        }
     }
+
+    // private void HandleRightClick(InputAction.CallbackContext ctx)
+    // {
+    //     Vector2 screenPosition = Mouse.current.position.ReadValue();
+    //     Vector2 worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
+    //     Debug.Log(grid.GetGridObject(worldPosition));
+    // }
 }
