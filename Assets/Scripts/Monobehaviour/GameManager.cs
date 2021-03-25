9a8fbadc51a92ffa220c5ca3d5e33134039f2fe0
ryfprivate <ryfprivate@outlook.com>
2021-03-25 20:40:16 +1100

    Separated main game scripts into game and map managers

--- Assets/Scripts/Monobehaviour/GameManager.cs
+++ Assets/Scripts/Monobehaviour/GameManager.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.Tilemaps;
 using UnityEngine.SceneManagement;
 using CodeMonkey.Utils;
 
@@ -18,28 +17,9 @@ public class GameManager : MonoBehaviour
     public GameObject[] lairPrefabs;
     public GameObject[] lairs;
 
-    public Tilemap baseTilemap;
-    public Tilemap spawnTilemap;
-    public Tilemap collidableTilemap;
-
-    public Tilemap lairTilemap;
-    public Tile lairTile;
-
-    public Tilemap pathTilemap;
-    public Tile pathTile;
-
-    // Class Instances
-    private Pathfinding _pathfinding;
-
-    // Spawning
-    private List<Vector3> lairLocations = new List<Vector3>();
-    private List<Vector3> spawnLocations = new List<Vector3>();
-    private Vector3 spawnOffset = new Vector3(.5f, .5f, 0);
-
-    private int mapSize = 50;
-
     // temp
     public GameObject simpleLairPrefab;
+    private Vector3 spawnOffset = new Vector3(.5f, .5f, 0);
 
     void OnEnable()
     {
@@ -55,16 +35,11 @@ public class GameManager : MonoBehaviour
         Instance = this;
 
         Controls = new GameControls();
-        _pathfinding = new Pathfinding(mapSize, mapSize);
     }
 
 
     void Start()
     {
-        // Adds all spawn locations to a list
-        // Adds all collidable tiles as obstacles in the pathfinding grid
-        spawnLocations = SetUpTiles();
-
         // Spawns all monster lairs
         // SpawnLairs();
 
@@ -82,64 +57,24 @@ public class GameManager : MonoBehaviour
         return Instantiate(obj, pos, Quaternion.Euler(Vector3.zero));
     }
 
-    private List<Vector3> SetUpTiles()
-    {
-        baseTilemap.CompressBounds();
-        BoundsInt bounds = baseTilemap.cellBounds;
-        Debug.Log(bounds);
-        // Tilemaps
-        TileBase[] spawnTiles = spawnTilemap.GetTilesBlock(bounds);
-        TileBase[] collidableTiles = collidableTilemap.GetTilesBlock(bounds);
-        TileBase[] lairTiles = lairTilemap.GetTilesBlock(bounds);
-        // Looks at every tile in grid
-        // If its a spawn or collidle tile, stores it in an array
-        for (int x = 0; x < mapSize; x++)
-        {
-            for (int y = 0; y < mapSize; y++)
-            {
-                TileBase spawnTile = spawnTiles[x + y * mapSize];
-                TileBase collidableTile = collidableTiles[x + y * mapSize];
-                TileBase lairTile = lairTiles[x + y * mapSize];
-
-                if (spawnTile != null)
-                {
-                    // Debug.Log("x:" + x + " y:" + y + " tile:" + spawnTile.name);
-                    spawnLocations.Add(new Vector3(x, y, 0));
-                }
-
-                if (collidableTile != null)
-                {
-                    _pathfinding.GetGrid().GetGridObject(x, y).isWalkable = false;
-                }
-
-                if (lairTile != null)
-                {
-                    lairLocations.Add(new Vector3(x, y, 0));
-                }
-            }
-        }
-
-        return spawnLocations;
-    }
-
     private void SpawnLairs()
     {
-        int numLairs = lairLocations.Count;
+        int numLairs = MapManager.Instance.lairLocations.Count;
         Debug.Log("num lairs " + numLairs);
         for (int i = 0; i < numLairs; i++)
         {
 
-            Vector3 lairSpawnPosition = lairLocations[i] + spawnOffset;
+            Vector3 lairSpawnPosition = MapManager.Instance.lairLocations[i] + spawnOffset;
             Spawn(simpleLairPrefab, lairSpawnPosition);
         }
     }
 
     private void SpawnPlayer()
     {
-        Debug.Log("num spawn " + spawnLocations.Count);
+        Debug.Log("num spawn " + MapManager.Instance.spawnLocations.Count);
         // Spawn player on random spawn location
         // Adds offset to position to spawn in middle of cell
-        Vector3 playerSpawnPosition = spawnLocations[Random.Range(0, spawnLocations.Count)] + spawnOffset;
+        Vector3 playerSpawnPosition = MapManager.Instance.spawnLocations[Random.Range(0, MapManager.Instance.spawnLocations.Count)] + spawnOffset;
         Spawn(playerPrefab, playerSpawnPosition);
     }
 }
--- Assets/Scripts/Monobehaviour/MapManager.cs
+++ Assets/Scripts/Monobehaviour/MapManager.cs
@@ -5,16 +5,37 @@ using UnityEngine.Tilemaps;
 
 public class MapManager : MonoBehaviour
 {
+    public static MapManager Instance { get; private set; }
+
     [SerializeField]
     private Tilemap baseTilemap;
+    [SerializeField]
+    private Tilemap spawnTilemap;
+    [SerializeField]
+    private Tilemap collidableTilemap;
+    [SerializeField]
+    private Tilemap lairTilemap;
 
+    // Spawning
+    public List<Vector3> lairLocations = new List<Vector3>();
+    public List<Vector3> spawnLocations = new List<Vector3>();
+
+    // Tile data
     [SerializeField]
     private List<TileData> tileDatas;
-
     private Dictionary<TileBase, TileData> dataFromTiles;
+
+    // Class Instances
+    private Pathfinding _pathfinding;
+
+    private int mapSize = 50;
 
     void Awake()
     {
+        Instance = this;
+
+        _pathfinding = new Pathfinding(mapSize, mapSize);
+
         dataFromTiles = new Dictionary<TileBase, TileData>();
 
         foreach (var tileData in tileDatas)
@@ -24,6 +45,13 @@ public class MapManager : MonoBehaviour
                 dataFromTiles.Add(tile, tileData);
             }
         }
+    }
+
+    void Start()
+    {
+        // Adds all spawn locations to a list
+        // Adds all collidable tiles as obstacles in the pathfinding grid
+        spawnLocations = SetUpTiles();
     }
 
     void Update()
@@ -42,5 +70,45 @@ public class MapManager : MonoBehaviour
                 PlayerManager.Instance.health -= 0.1f;
             }
         }
+    }
+
+    private List<Vector3> SetUpTiles()
+    {
+        baseTilemap.CompressBounds();
+        BoundsInt bounds = baseTilemap.cellBounds;
+        Debug.Log(bounds);
+        // Tilemaps
+        TileBase[] spawnTiles = spawnTilemap.GetTilesBlock(bounds);
+        TileBase[] collidableTiles = collidableTilemap.GetTilesBlock(bounds);
+        TileBase[] lairTiles = lairTilemap.GetTilesBlock(bounds);
+        // Looks at every tile in grid
+        // If its a spawn or collidle tile, stores it in an array
+        for (int x = 0; x < mapSize; x++)
+        {
+            for (int y = 0; y < mapSize; y++)
+            {
+                TileBase spawnTile = spawnTiles[x + y * mapSize];
+                TileBase collidableTile = collidableTiles[x + y * mapSize];
+                TileBase lairTile = lairTiles[x + y * mapSize];
+
+                if (spawnTile != null)
+                {
+                    // Debug.Log("x:" + x + " y:" + y + " tile:" + spawnTile.name);
+                    spawnLocations.Add(new Vector3(x, y, 0));
+                }
+
+                if (collidableTile != null)
+                {
+                    _pathfinding.GetGrid().GetGridObject(x, y).isWalkable = false;
+                }
+
+                if (lairTile != null)
+                {
+                    lairLocations.Add(new Vector3(x, y, 0));
+                }
+            }
+        }
+
+        return spawnLocations;
     }
 }
